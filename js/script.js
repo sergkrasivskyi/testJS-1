dd9c9e6d0116516f0ed26b30734399500e46f947
sergkrasivskyi <sergii.krasivskyi@gmail.com>
2022-06-12 14:26:28 +0300

    idea

--- js/script.js
+++ js/script.js
@@ -1,39 +1,34 @@
 // alert(window.innerHeight)
-
-if (window.location.pathname == "/index.html") {
-const currentStudio = document.querySelector(".current-studio");
-//   current.style.backgroundColor = "blue";
+alert(window.location.pathname)
+if (window.location.pathname == "$index.html") {
+  const currentStudio = document.querySelector(".current-studio");
+  //   current.style.backgroundColor = "blue";
   currentStudio.style.color = "blue";
-  currentStudio.style.fontSize = '40px'
-//   text.style.fontSize = '40px'
+  currentStudio.style.fontSize = "40px";
+  //   text.style.fontSize = '40px'
 }
-if (window.location.pathname == "/txt.html") {
-    const currentPortpholio = document.querySelector(".current-portpholio");
-    // text.style.backgroundColor = "green";
-    currentPortpholio.style.color = "green";
-    currentPortpholio.style.fontSize = '40px'
-  }
+if (window.location.pathname == "$txt.html") {
+  const currentPortpholio = document.querySelector(".current-portpholio");
+  // text.style.backgroundColor = "green";
+  currentPortpholio.style.color = "green";
+  currentPortpholio.style.fontSize = "40px";
+}
 
 //   const link = document.querySelector('.link');
 //   link.style.borderBottom = '2px solid red'
 //   setTimeout(() => document.body.style.background = "", 5000);
 //   for (let i = 1; i<=5; i= i+ 1) {
-    // setTimeout(() => text.style.backgroundColor = "blue", 3000);
-    // text.style.color = "green";
-    // text.style.fontSize = '40px'
-    // setTimeout(() => text.style.backgroundColor = "yellow", 3000);
-    // text.style.color = "blue";
+// setTimeout(() => text.style.backgroundColor = "blue", 3000);
+// text.style.color = "green";
+// text.style.fontSize = '40px'
+// setTimeout(() => text.style.backgroundColor = "yellow", 3000);
+// text.style.color = "blue";
 // }
 //   const paragraph = document.querySelector(".paragraph");
 //   paragraph.style.backgroundColor = "red";
 // }
 
-
-
-
-
-
-// 
+//
 // const button = document.querySelector('button')
 
 // var elem = document.querySelector('#studio-link');
